e56616cf3088be2b537e2a93073ccca5412a5a95
HyungJinHan <han1210_36@naver.com>
2022-09-19 21:51:43 +0900

    test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import background from "./main/videos/mainvideo.mp4";
 import "./App.css";
 
 function App() {
-  //tq
+  //tq 욕함?
   return (
     <div className="mainbody">
       <div className="mainvideo">
