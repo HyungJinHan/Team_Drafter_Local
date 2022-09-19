1fe41681adbdf500dca33bd538ade011f990cb22
김찬진 <aqs450@gmail.com>
2022-09-19 21:29:09 +0900

    살려줘

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import background from "./main/videos/mainvideo.mp4";
 import "./App.css";
 
 function App() {
-  //난 머지안됨?
   return (
     <div className="mainbody">
       <div className="mainvideo">
