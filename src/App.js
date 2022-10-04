09f256f30e5d242d176d89cd010b92e94d153921
HyungJin Han <han1210_36@naver.com>
2022-10-04 21:33:11 +0900

    test

--- src/App.js
+++ src/App.js
@@ -13,35 +13,35 @@ import AuctionCreate from "./auction/AuctionCreate";
 function App() {
   return (
     //아무거나 입력 중
-    
-    // <div className="mainBody">
-    //   <div className="mainVideo">
-    //     <video loop autoPlay muted>
-    //       <source src={background} type="video/mp4" />
-    //     </video>
-    //     <div className="appBody">
-    //       <Routes>
-    //         <Route path="/" element={<LeaderLogin />} />
-    //         <Route path="/leaderjoin" element={<LeaderJoin />} />
-    //         <Route path="/memberlogin" element={<MemberLogin />} />
-    //         <Route path="/memberjoin" element={<MemberJoin />} />
-    //         <Route path="/main" element={<Main />} />
-    //         <Route path="/auction" element={<Auction />} />
-    //         <Route path="/auction/App" element={<AuctionList />} />
-    //         <Route path="/auction/JSA" element={<AuctionList />} />
-    //         <Route path="/auction/JSB" element={<AuctionList />} />
-    //         <Route path="/auction/SprA" element={<AuctionList />} />
-    //         <Route path="/auction/SprB" element={<AuctionList />} />
-    //         <Route path="/auction/SAMUL" element={<AuctionList />} />
-    //         <Route path="/auction/SIGAK" element={<AuctionList />} />
-    //         <Route path="/auction/UNUH" element={<AuctionList />} />
-    //         <Route path="/auction/CLDA" element={<AuctionList />} />
-    //         <Route path="/auction/CLDB" element={<AuctionList />} />
-    //         <Route path="/auction/create/JSA" element={<AuctionCreate />} />
-    //       </Routes>
-    //     </div>
-    //   </div>
-    // </div>
+
+    <div className="mainBody">
+      <div className="mainVideo">
+        <video loop autoPlay muted>
+          <source src={background} type="video/mp4" />
+        </video>
+        <div className="appBody">
+          <Routes>
+            <Route path="/" element={<LeaderLogin />} />
+            <Route path="/leaderjoin" element={<LeaderJoin />} />
+            <Route path="/memberlogin" element={<MemberLogin />} />
+            <Route path="/memberjoin" element={<MemberJoin />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/auction" element={<Auction />} />
+            <Route path="/auction/App" element={<AuctionList />} />
+            <Route path="/auction/JSA" element={<AuctionList />} />
+            <Route path="/auction/JSB" element={<AuctionList />} />
+            <Route path="/auction/SprA" element={<AuctionList />} />
+            <Route path="/auction/SprB" element={<AuctionList />} />
+            <Route path="/auction/SAMUL" element={<AuctionList />} />
+            <Route path="/auction/SIGAK" element={<AuctionList />} />
+            <Route path="/auction/UNUH" element={<AuctionList />} />
+            <Route path="/auction/CLDA" element={<AuctionList />} />
+            <Route path="/auction/CLDB" element={<AuctionList />} />
+            <Route path="/auction/create/JSA" element={<AuctionCreate />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
